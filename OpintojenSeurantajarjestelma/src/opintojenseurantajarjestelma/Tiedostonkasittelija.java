19ab9f2d3617b6d1c333a34939525bf3ba505d62
Juha  Koponen <Koppa@87-95-89-183.bb.dnainternet.fi>
2012-12-22 15:53:14 +0200

    tiedostonkäsittelijä lisätty

---
+++ OpintojenSeurantajarjestelma/src/opintojenseurantajarjestelma/Tiedostonkasittelija.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opintojenseurantajarjestelma;
+import java.util.*;
+
+/**
+ *
+ * @author Koppa
+ */
+public class Tiedostonkasittelija {
+    
+    public void tallenna(String tiedosto, String teksti) {
+        
+    }
+    
+    public ArrayList<String> lue() {
+        ArrayList<String> luettu = new ArrayList<String>();
+        return luettu;
+    }
+    
+}
